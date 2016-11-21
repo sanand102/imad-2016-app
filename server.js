de8ee493ef021a4cd191cc559f6d3ced07c2d608
sanand102 <sanand.sivan@gmail.com>
2016-11-21 12:22:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 
+app.get('/article-three.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 
 
 
